c55c933b60e8812b876f51d7d635feaae32f02cc
Abula <nikaabuladze28@gmail.com>
2022-11-23 23:19:37 +0400

    first commit

--- app.js
+++ app.js
@@ -9,5 +9,8 @@ sumBtn.addEventListener('click', function(){
     answerDiv.textContent = `${answer}`
     if(firstInp.value == '' && secondInp.value == ''){
         answerDiv.textContent = '0'
+    }else if (firstInp.value == '' || secondInp.value == ''){
+        answerDiv.textContent = "Fill All Number"
+        answerDiv.textContent.style.color = 'red'
     }
 })
